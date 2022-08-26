be777b64d8b0664945f1a1f630a446f6f50852ef
vinnu23 <85779539+vinnu23@users.noreply.github.com>
2022-08-26 05:37:36 +0530

    testing vercel serverless functions with random joke api

--- api/index.go
+++ api/index.go
@@ -1,10 +1,18 @@
 package handler
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	resp, err := http.Get("https://v2.jokeapi.dev/joke/Any")
+	if err != nil {
+		fmt.Println("Error is req: ", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body) // response body is []byte
+	fmt.Println(string(body))
+	fmt.Fprintf(w, string(body))
 }
