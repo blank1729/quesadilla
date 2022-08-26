bc0f94cd4958f00aa11da00635b204493896298c
vinnu23 <85779539+vinnu23@users.noreply.github.com>
2022-08-26 06:04:52 +0530

    testing vercel serverless functions with random joke api take #3

--- api/index.go
+++ api/index.go
@@ -1,10 +1,29 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
+
+type Data struct {
+	err      bool
+	category string
+	joketype string
+	Joke     string
+	flags    struct {
+		p1 bool
+		p2 bool
+		p3 bool
+		p4 bool
+		p5 bool
+		p6 bool
+	}
+	id   int
+	safe bool
+	lang string
+}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	resp, err := http.Get("https://v2.jokeapi.dev/joke/Any")
@@ -13,6 +32,14 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body) // response body is []byte
-	fmt.Println(string(body))
-	fmt.Fprintf(w, string(body))
+	// fmt.Println(string(body))
+	data := string(body)
+	var payload Data
+	err = json.Unmarshal([]byte(data), &payload)
+	if err != nil {
+
+	}
+	// fmt.Println("this is me testing 1,2,3")
+	fmt.Printf("Joke: %s\n", payload.Joke)
+	fmt.Fprintf(w, payload.Joke)
 }
